4a747405586bb6c0293fd35ec9eed7e42ae247a0
snokpok <vinceny.fe@gmail.com>
2023-11-12 14:41:12 -0800

    change color and format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config'; // import lit from '@astrojs/lit';
+import { defineConfig } from "astro/config"; // import lit from '@astrojs/lit';
 
-import sitemap from '@astrojs/sitemap';
-
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-minimal-starter.netlify.app/",
   integrations: [sitemap()],
   redirects: {
-    "/zoom": "https://usc.zoom.us/j/3359916674?pwd=RklnSDg4YjVZNENUa013aDEwY0J4dz09",
-    "/chat": "https://cal.com/snokpok"
-  }
+    "/zoom":
+      "https://usc.zoom.us/j/3359916674?pwd=RklnSDg4YjVZNENUa013aDEwY0J4dz09",
+    "/chat": "https://cal.com/snokpok",
+  },
 });
