21e317793f5ed4ee71e9c507402a74cd1b7e996c
snokpok <vinceny.fe@gmail.com>
2024-02-28 11:01:02 -0800

    change cal link

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   redirects: {
     "/zoom":
       "https://usc.zoom.us/j/3359916674?pwd=RklnSDg4YjVZNENUa013aDEwY0J4dz09",
-    "/chat": "https://booking.akiflow.com/snokpok",
-    "/cal": "https://booking.akiflow.com/snokpok",
+    "/chat": "https://cal.com/snokpok/30min",
+    "/cal": "https://cal.com/snokpok/30min",
   },
 });
