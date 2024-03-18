5671ae0d00847f6d699b99e2838ff51c68f0dfab
snokpok <vinceny.fe@gmail.com>
2024-03-17 21:22:00 -0700

    oopsee

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config"; // import lit from '@astrojs/lit';
 
 import sitemap from "@astrojs/sitemap";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vuvincent.com",
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
   redirects: {
     "/zoom":
       "https://usc.zoom.us/j/3359916674?pwd=RklnSDg4YjVZNENUa013aDEwY0J4dz09",
---
+++ src/components/SCP.tsx
@@ -0,0 +1,29 @@
+import type { FC } from "react";
+
+let songName: string, artists: string;
+
+const accessToken = process.env.SCP_TOKEN;
+const response = await fetch("https://scp-go-production.up.railway.app/scp", {
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
+});
+const data: any | null = await response.json();
+songName = data?.item.name ?? null;
+artists = data?.item.artists.map((el) => el.name).join(", ") ?? null;
+
+const SCP: FC<{}> = () => {
+  return (
+    <>
+      {data !== null && (
+        <>
+          <hr />
+          <p>
+            🎵 Btw I'm listening to: <strong>{songName}</strong> - {artists}
+          </p>
+        </>
+      )}
+    </>
+  );
+};
+export default SCP;
